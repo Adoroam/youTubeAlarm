16b63c6467a2730368a1ee9f6d0692065dd24053
Anthony Stabile <AStabile.Design@gmail.com>
2017-09-20 20:49:40 -0500

    added inputs to type the time you want it to go off and streamlined functions

--- youTubeAlarm.js
+++ youTubeAlarm.js
@@ -1,16 +1,15 @@
-const a = {}
-a.clicked = false
-a.hour = 6
-a.minutes = 30
-let success = () => {
+let inputs = document.createElement('div')
+inputs.innerHTML = '<input type="number" placeholder="hour" id="hour" max="24" style="display: inline; width: 50px; margin-left: 1em;"><input type="number" placeholder="mintues" id="minutes" max="59" style="display: inline; width: 80px;">'
+document.getElementById('search').appendChild(inputs)
+
+let setHour = () => { return Number(document.getElementById('hour').value) }
+let setMinutes = () => { return Number(document.getElementById('minutes').value) }
+let startAlarm = () => {
   document.getElementsByClassName('ytp-play-button')[0].click()
-  a.clicked = true
   clearInterval(timer)
 }
-let theLoop = () => {
-  if (!a.clicked) {
-    let current = new Date()
-    if (current.getHours() === a.hour && current.getMinutes() === a.minutes) success()
-  }
-}
+let c = () => { return new Date() }
+let checkHour = () => { return c().getHours() === setHour() }
+let checkMinutes = () => { return c().getMinutes() === setMinutes() }
+let theLoop = () => { if (checkHour() && checkMinutes()) startAlarm() }
 let timer = setInterval(theLoop, 1000)
